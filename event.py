1b34bebf304d9a40aea244003c11d18a9c76d4a8
Alvaro Santamaria Herrero <aluarosi@gmail.com>
2013-07-11 18:15:20 +0200

    reactor module refactoring & setTimeout & setInterval

--- event.py
+++ event.py
@@ -1,8 +1,13 @@
-# TODO: look at nodejs code lib/event.js
-# error event...
 class EventEmitter( object ):
 
     def addListener( self, event_type, callback ):
+        """ Adds listener callback function for event_type 
+            
+            callback function has the signature
+                callback( emitter, data )  
+                    emitter :   instance of EventEmitter
+                    data    :   data passed along  
+        """
         if not hasattr( self, 'listeners' ):
             self.listeners = {} 
         if not self.listeners.has_key( event_type ):
@@ -10,10 +15,13 @@ class EventEmitter( object ):
         if callback not in self.listeners[event_type]:
             self.listeners[event_type].append( callback )
 
+    # Alias for addListerner 
     on = addListener
 
     def removeListener( self, event_type, callback ):
-        if  hasattr( self, 'listeners' ) and self.listeners.has_key( event_type ) and callback in self.listeners[event_type]:
+        if  hasattr( self, 'listeners' )\
+        and self.listeners.has_key( event_type )\
+        and callback in self.listeners[event_type]:
             self.listeners[event_type].remove( callback  )
 
     def removeAllListeners( self, event_type ):
@@ -21,16 +29,19 @@ class EventEmitter( object ):
             del self.listeners[event_type][:]   #Empties the list
 
     def emit( self, event_type, event_data ):
+        """ Emits event, calling listening callbacks
+            with parameters:    callback( event_emmitter, event_data)
+            (Consider if passing along event_type is worth it) 
+        """
         for callback in self.listeners[event_type]:
-            callback( self, event_type, event_data ) 
-
-    
+            callback( self, event_data ) 
 
 
 if __name__ == "__main__":
 
+    """ Very basic testing code """
+
     class TestEventEmitter(EventEmitter):
-    
         def __init__(self):
             print type(self)
     
--- reactor.py
+++ reactor.py
@@ -2,33 +2,142 @@ import event
 import time
 
 class Reactor(event.EventEmitter):
+    """ Reactor (i.e. "Event Loop") 
+        
+        Coded to help understand:
+            - Asynchronous programming
+            - Event emitter/listener pattern
+            - Event driven programming
+            - Callbacks (Continuation-passing style, CPS)
+
+        Inspired on nodejs
+        Minimalistic approach: only code what is needed to understand concepts
+    """
 
     def __init__(self):
         pass
 
     def run(self):
         """ Event reactor loop """
-        while hasattr(self,'listeners') and self.listeners.has_key('tick') and self.listeners['tick']:
+        while hasattr(self,'listeners') and self.listeners.has_key('tick')\
+                                        and self.listeners['tick']:
             # Tick event
             self.emit('tick', time.time())
 
+# Reactor as a global object
+reactor = Reactor()     
 
-reactor = Reactor()     # Reactor as a global object
 
+class ReactorAttachable(event.EventEmitter):
+    """ Class for objects that can be attached to the reactor """
+
+    def set(self):
+        reactor.on('tick', self.handleTick)
+        return self 
+
+    def unset(self):
+        reactor.removeListener('tick', self.handleTick)
+        return self
+
+
+class Timeout(ReactorAttachable):
+    def __init__(self, seconds):
+        """ seconds :   delay of the timeout 
+        """
+        self.seconds = seconds
+        self.old_time = 0
+        self.flag = False
+
+    def handleTick(self, emitter, data):
+        """ Emmits event
+            Event type  :   'timeout'
+            Event data  :   time in seconds
+        """
+        new_time = time.time()
+        if new_time - self.old_time > self.seconds:
+            if not self.flag:
+                self.old_time = new_time 
+                self.flag = True
+            else:
+                self.unset()
+                self.emit('timeout', self.seconds)
+
+    @staticmethod
+    def setTimeout(callback, seconds=1.0):
+        """ Wrapper function 
+            calls calback() 
+        """
+        def event_callback( emitter, data):
+            """ Adapts function signatures 
+                If we decide not to call back without 
+                event_emitter or event_data
+                (Not used)
+            """
+            return callback() 
+        timeout = Timeout(seconds).set()
+        timeout.on('timeout', callback)        
+        return timeout
+
+    @staticmethod
+    def clearTimeout(timeout):
+        timeout.unset()
+        return timeout
+
+
+class Interval(ReactorAttachable):
+    def __init__(self, seconds):
+        """ seconds :   delay of the interval
+        """  
+        self.seconds = seconds
+        self.old_time = 0
+        self.count = 0
+        
+    def handleTick(self, emitter, data):
+        """ Emmits event
+            Event type  :   'interval'
+            Event data  :   count of intervals so far
+        """
+        new_time = time.time()
+        if new_time - self.old_time > self.seconds:
+            self.old_time = new_time 
+            self.count += 1 
+            self.emit('interval', self.count)
+
+    @staticmethod
+    def setInterval(callback, seconds=1.0):
+        """ Wrapper function 
+            calback( emitter, count)
+        """
+        interval = Interval(seconds).set() 
+        interval.on('interval', callback)
+        return interval 
+
+    @staticmethod
+    def clearInterval(interval):
+        interval.unset()
+        return interval
+
+# Publish static methods
+setTimeout = Timeout.setTimeout
+clearTimeout = Timeout.clearTimeout
+setInterval = Interval.setInterval
+clearInterval = Interval.clearInterval
 
 if __name__ == "__main__":
 
+    """ Basic example of usage and experiments """
+
     # Register a simple function for each tick
-    def handleTick(emitter, event_type, event_data):
+    def my_handleTick(emitter, event_type, event_data):
         print "%s %s" % (event_type, event_data)
         # Block for a while
         import time
         time.sleep(1) 
 
     # Interval, based on closure with problem (scope old_time)
-    def setInterval(interval, doInterval):
+    def my_setInterval(interval, doInterval):
         reactor.old_time = time.time()
-        def handleTick(e, t, d):
+        def handleTick(e, d):
             new_time = time.time()
             if new_time-reactor.old_time > interval:
                 # NO puedo actualizar old_time en el scope superior!!!!
@@ -38,85 +147,28 @@ if __name__ == "__main__":
                  
         reactor.on('tick', handleTick)
 
-    # Interval, based on object
-    class Interval(event.EventEmitter):
-        INTERVAL = 'interval'
-        def __init__(self, seconds, callback):
-            self.seconds = seconds
-            self.old_time = 0
-            self.on(self.INTERVAL, callback)  # TODO: --> take away from here
-        
-        def set(self):
-            reactor.on('tick', self._handleTick)
-            return self
-
-        def unset(self):
-            reactor.removeListener('tick', self._handleTick)
-
-        def _handleTick(self, e, t, d):
-            new_time = time.time()
-            if new_time - self.old_time > self.seconds:
-                self.old_time = new_time
-                self.emit(self.INTERVAL, self.seconds)
-
-        # Convenience class method
-        @staticmethod
-        def setInterval(seconds, callback):
-            return Interval(seconds, callback).set() # .... and fix this 
-     
-    
-    class Timeout(event.EventEmitter):
-        def __init__(self, seconds):
-            self.seconds = seconds
-            self.old_time = 0
-            self.flag = False
-
-        def set(self):
-            reactor.on('tick', self._handleTick)
-            return self   
-
-        def unset(self):
-            reactor.removeListener('tick', self._handleTick)
-    
-        def _handleTick(self, e, t, d):
-            new_time = time.time() 
-            if new_time - self.old_time > self.seconds:
-                if not self.flag:
-                    self.old_time = new_time
-                    self.flag = True 
-                else:
-                    reactor.removeListener('tick', self._handleTick)
-                    self.emit('timeout', self.seconds)
-
-        @staticmethod
-        def setTimeout(seconds, callback):
-            timeout = Timeout(seconds).set()
-            timeout.on('timeout', callback)
-            return timeout
-                   
 
     # Connect everyting
-    #reactor.on('tick', handleTick) 
-    def doit(e,t,d):
+    def doit(emitter, data):
         print "timer..."
-    #setInterval(2,doit)
-    i = Interval.setInterval(1.5,doit)
-    print i 
-    i.unset()
-    print i.listeners
-    i.set()
-    #i.removeListener(i.INTERVAL ,doit)
-    print i.listeners
-    print reactor.listeners
-    #i.unset()
+        global tt
+        tt = setTimeout(doit, 1)
+    def cancel(emitter, data):
+        print "clear"
+        clearTimeout(tt)
+        clearInterval(i)
+         
+    def doit2(emitter, count):
+        print "interval...%s" %count
 
-    def cancel_interval(i):
-        def cb(e,t,d):
-            i.unset()
-            print "Interval canceled"
-        return cb
-    # TODO --> This is clumsy, if each callback has to have emitter, event_type, event_data...
-    t = Timeout.setTimeout(4, cancel_interval(i));
+    def block(emitter, data):
+        print "Reactor blocked!"
+        while True:
+            pass    
 
+    t = setTimeout(doit, seconds=1) 
+    setTimeout(cancel, 10) 
+    i = setInterval(doit2, 0.7) 
+    setTimeout(block, 5) 
     # Main Loop
     reactor.run()
